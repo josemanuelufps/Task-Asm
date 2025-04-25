5dc5d98e1d5a0b9c8dd90ebe1aac802b95ebd5e9
José Manuel Pérez Rodríguez <josemanuelpr@ufps.edu.co>
2025-04-24 21:15:49 -0500

    Updated main.asm with some tests for the utils file

--- src/main.asm
+++ src/main.asm
@@ -1,75 +1,94 @@
 ; ----------------------------------------------------
 ; Authors: josemanuelpr@ufps.edu.co
 ;          andresfelipemp@ufps.edu.co
-; Description: Load tasks from a comma-separated CSV and print the description of the first task
 ; ----------------------------------------------------
 
+; ----------------------------------------------------
+; How to use (with Masm.exe and Link.exe):
+; run these commands (include the last ";", this skips the step of naming the output file))
+
+; Masm.exe main.asm ; 
+; Masm.exe utils.asm ;
+; Link.exe main.obj utils.obj ;
+; main.exe
+
+; Keep an eye that the "text.txt" file must be in the same directory as the executable.
+; ----------------------------------------------------
 .model small
 .stack 100h
 
+
+extrn open_file:near, close_file:near, read_file:near
+extrn print_str:near, print_chr:near, print_number:near, print_newline:near
+
+
 .data
-    filename    db 'text.txt', 0    ; File name WITH NULL TERMINATOR
-    filehandle  dw ?                ; File handler
-    buffer      db 4096 dup('$')    ; Read buffer (ENDING WITH $)
-    err_msg     db 'Error!', 13, 10, '$'
+    filename    db 'text.txt', 0
+    filehandle  dw ?
+    buffer      db 4096 dup('$')  ; Read buffer
     success_msg db 'File contents:', 13, 10, '$'
+    error_msg   db 'Error!', 13, 10, '$'
 
 .code
-main proc
-    mov ax, @data
-    mov ds, ax
-    mov es, ax
+    main proc
+        mov ax, @data
+        mov ds, ax
 
-    ; Opening the file
-    mov ah, 3Dh         ; DOS open file function (3Dh)
-    mov al, 0           ; Reading mode in al
-    mov dx, offset filename
-    int 21h
-    jc error            ; Jump if error (CF==1)
+        ; 1. Open file
+        lea dx, filename
+        call open_file
+        jc @error
+        mov [filehandle], ax
 
-    mov [filehandle], ax ; Save file handle
+        ; 2. Read file
+        mov bx, [filehandle]
+        mov cx, 4095       ; Max bytes to read (leave space for '$')
+        lea dx, buffer
+        call read_file
+        jc @error
 
-    ; Reading the file
-    mov ah, 3Fh         ; DOS read file function
-    mov bx, [filehandle]
-    mov cx, 4095        ; Read maximum 4095 bytes
-    mov dx, offset buffer
-    int 21h
-    jc error            ; Jump if error
+        ; 3. Close file
+        mov bx, [filehandle]
+        call close_file
+        jc @error
 
-    ; Add string terminator
-    mov si, offset buffer
-    add si, ax          ; AX contains bytes read
-    mov byte ptr [si], '$'
+        ; 4. Print success message + file content
+        lea dx, success_msg
+        call print_str
 
-    ; Close file
-    mov ah, 3Eh         ; DOS close file function
-    mov bx, [filehandle]
-    int 21h
+        lea dx, buffer
+        call print_str
 
-    ; Display success message
-    mov ah, 09h
-    mov dx, offset success_msg
-    int 21h
+        ; 5. Also, let's print the numbers from 10 to -2 cuz why not
+        mov cx, 10       ; Start value (instead of counter)
+    @print_numbers:
+        mov ax, cx      ; Current number to print
+        call print_number
+        
+        ; Print space between numbers
+        mov dl, ' '
+        call print_chr
+        
+        dec cx           ; Move to next number
+        cmp cx, -3       ; We want to stop after -2
+        jg @print_numbers ; Jump if greater than -3 (stops after -2)
+        
+        ; Print newline after all numbers
+        call print_newline
 
-    ; Display file contents
-    mov ah, 09h
-    mov dx, offset buffer
-    int 21h
+        jmp @finish_program
 
-    ; Exit
-    mov ax, 4C00h
-    int 21h
+    @error:
+        mov ah, 09h
+        lea dx, error_msg
+        int 21h
+        mov ax, 4C01h      ; Exit with error code
+        int 21h
 
-error:
-    ; Display error message
-    mov ah, 09h
-    mov dx, offset err_msg
-    int 21h
-
-    ; Exit with error
-    mov ax, 4C01h
-    int 21h
-
-main endp
+    @finish_program:
+        ; Exit (success)
+        mov ax, 4C00h
+        int 21h
+    
+    main endp
 end main
